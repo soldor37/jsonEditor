78ef770974fd27e3b2d43d74671b32f14b0206e6
soldor37 <seventhpirate@gmail.com>
2020-07-10 17:51:11 +0300

    init editor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JsonInputComponent } from './jsonInput/jsonInput.component';
+import { JsonEditorComponent } from './json-editor/json-editor.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JsonInputComponent,
+    JsonEditorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/jsonInput/jsonInput.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'app-jsonInput',
+    templateUrl: './jsonInput.component.html',
+    styleUrls: ['./jsonInput.component.css']
+})
+export class JsonInputComponent {}
