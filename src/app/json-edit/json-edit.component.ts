cbf37970060c2f44f5abab1f11d9f5c9d865a9e8
soldor37 <seventhpirate@gmail.com>
2020-07-11 14:24:25 +0300

    get table from json obj

--- src/app/json-edit/json-edit.component.ts
+++ src/app/json-edit/json-edit.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./json-edit.component.css']
 })
 export class JsonEditComponent implements OnInit {
+  tmpJson = [
+    {
+      "name": "Name 1",
+      "year": "2001"
+    },
+    {
+      "name": "Name 2",
+      "year": "2002"
+    },
+    {
+      "name": "Name 3",
+      "year": "2003"
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
