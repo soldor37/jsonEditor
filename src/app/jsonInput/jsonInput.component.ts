0083658067b3dbcd8457d717222692be6b653ea2
soldor37 <seventhpirate@gmail.com>
2020-07-14 09:59:00 +0300

    input from txt file

--- src/app/jsonInput/jsonInput.component.ts
+++ src/app/jsonInput/jsonInput.component.ts
@@ -17,6 +17,23 @@ export class JsonInputComponent {
         this.jsonObj = JSON.parse(textVar);
         this.jsonIn.emit(this.jsonObj);
     }
+
+    public changeListener(files: FileList){
+        console.log(files);
+        if(files && files.length > 0) {
+           let file : File = files.item(0); 
+            //  console.log(file.name);
+            //  console.log(file.size);
+            //  console.log(file.type);
+             let reader: FileReader = new FileReader();
+             reader.readAsText(file);
+             reader.onload = (e) => {
+                let txt: string = reader.result as string;
+                this.textInput = txt;
+                // console.log(txt);
+             }
+          }
+      }
     // jsonObj = {};
     // jsonToObg(textVar): void{
     //     this.jsonObj = {};
